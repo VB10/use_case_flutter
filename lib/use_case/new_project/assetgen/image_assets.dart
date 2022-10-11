ed8823017759bac43dad57753c0fac32ae8df0ca
Veli Bacik <vbacik.10@gmail.com>
2022-10-11 03:46:13 +0300

    added structre

---
+++ lib/use_case/new_project/assetgen/image_assets.dart
@@ -0,0 +1,6 @@
+enum AssetImages {
+  profile('asset/images/profile.png');
+
+  final String value;
+  const AssetImages(this.value);
+}
---
+++ lib/use_case/new_project/src/product/space_items.dart
@@ -0,0 +1,7 @@
+enum SpaceItems {
+  low(10),
+  mid(15);
+
+  final double value;
+  const SpaceItems(this.value);
+}
