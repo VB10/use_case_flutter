23e3c9af98a10250357b8b084831c55cdf6a1c65
vb10 <vbacik.10@gmail.com>
2023-02-03 05:03:21 +0300

    added latest changes

--- lib/use_case/social/core/social_button.dart
+++ lib/use_case/social/core/social_button.dart
@@ -2,28 +2,19 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:use_case_flutter/use_case/social/core/social_adapter.dart';
 
-class SocialButton extends StatelessWidget {
+class SocialButton<T extends SocialAdapter> extends StatelessWidget {
   const SocialButton({
     super.key,
     required this.adapter,
     required this.onCompleted,
   });
-  final SocialAdapter adapter;
+  final T adapter;
   final void Function(String token) onCompleted;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
-      onPressed: () async {
-        // final response = await adapter.make.call();
-        final response2 = await GlobalException.make<String>(adapter.make);
-        if (response2 == null || response2.isEmpty) {
-          // push to login view
-          return;
-        }
-        // response ?? '';
-        onCompleted.call(response2);
-      },
+      onPressed: () async => onCompleted.call(await adapter.make.call() ?? ''),
       style: ElevatedButton.styleFrom(
         backgroundColor: adapter.model.color,
       ),
@@ -32,6 +23,15 @@ class SocialButton extends StatelessWidget {
     );
   }
 }
+
+// final response = await adapter.make.call();
+// final response2 = await GlobalException.make<String>(adapter.make);
+// void if (response2 == null || response2.isEmpty) {
+//   // push to login view
+//   return;
+// }
+// // response ?? '';
+// onCompleted.call(response2);
 
 class GlobalException {
   static Future<T?> make<T>(AsyncValueGetter<T?> onOperation) async {
